d403fd7514ec5081a1781d466c8fd0c732988629
Hubert Yang <yngsbyh@gmail.com>
2022-01-19 23:52:23 -0600

    bruh

--- docs/modular.md
+++ docs/modular.md
@@ -6,13 +6,16 @@
 
 * Constants:
     - `P`: Desired modulus; change accordingly
-* Class functions and supported operations:
+* Class functions:
     - `inv()`: Returns the modular inverse of the current object
 * Utility:
-    - `init(int n)`: Computes the factorial `fac[i]` and inverse factorial `iFac[i]` for all integers `i` from `[0...n]`; NOTE: must be called before `fac`, `iFac`, and `C(int n, int k)` can be used
+    - `init(int n)`: Computes the factorial `fac[i]` and inverse factorial `iFac[i]` for all integers `i` from `[0...n]`; **must be called before** `fac`, `iFac`, **and** `C(int n, int k)` **can be used**
     - `fac[i]`: Stores the factorial of `i` modulo `P`
 	- `iFac[i]`: Stores the inverse factorial of `i` modulo `P`
     - `C(int n, int k)`: Returns `n` choose `k` modulo `P`
+*
+
+**NOTE**: operations work under the assumption that `P` is prime
 
 [//]: # (Force end list)
 ### Example
--- examples/modular.cpp
+++ examples/modular.cpp
@@ -8,27 +8,6 @@ using i64 = long long;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
-    /*
-    objects of type Z support arithmetic operations under a modulus P
-
-    init(int n):            computes the factorial and inverse factorial of all integers from [0...n];
-                            call before using C(int n, int k);
-
-    fac[i]:                 stores i! modulo P
-
-    iFac[i]:                stores modular inverse of i! modulo P
-
-    Z C(int n, int k):      returns 0 if k < 0 or k > n; returns (n choose k) modulo P otherwise
-
-    Z inv():                returns the modular inverse of an object Z
-
-    power(T a, int b):      returns a ^ b modulo P in log(b) time;
-                            note: if the result is large, cast a as type Z
-
-    note: adjust the value of the modulus P as needed
-    warning: operations work under the assumption that P is prime
-    */
 
     Z two = 2;
     cout << two << '\n';              // output: 2
